da154944726edda66b21435a1ba60258d0932cef
Sergey Reymerov <sergeyreym@gmail.com>
2016-04-21 00:08:29 +0800

    Added database

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -1,14 +1,28 @@
 package service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @RequestMapping("/")
     String hello() {
-        return "hello world";
+
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS customers(" +
+                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+        Integer v = jdbcTemplate.query(
+                "SELECT 2+2 as res",
+                new Object[] { },
+                (rs, rowNum) -> new Integer(rs.getInt("res"))
+        ).get(0);
+
+        return "hello world " + v;
     }
 }
